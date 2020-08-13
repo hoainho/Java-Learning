56cafcf853a767832fa40d9ba236ffa789f4efc8
RemAlw <nhoxtvt@gmail.com>
2020-08-13 16:23:10 +0700

    Add value for Lab06

--- Lab06/Bai01/src/HINHVUONG.java
+++ Lab06/Bai01/src/HINHVUONG.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class HINHVUONG {
+    Scanner sc = new Scanner(System.in);
+    private double canh;
+    public double getPi() {
+        return canh;
+    }
+
+    public void setPi(double pi) {
+        this.canh = pi;
+    }
+    public HINHVUONG(){
+        canh = 0;
+    }
+    public HINHVUONG(double pi){
+        this.canh = pi;
+    }
+    public void input(){
+        System.out.println("Nhap Pi : ");
+        canh = sc.nextDouble();
+    }
+    public void output(){
+        System.out.println("\t\t\t[+] Pi : " + canh);
+    }
 }

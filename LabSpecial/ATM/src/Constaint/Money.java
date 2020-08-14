d4ef7b3e975fe7d2de9bbdff353adc1b6dac9d8c
RemAlw <nhoxtvt@gmail.com>
2020-08-14 22:10:37 +0700

    1 project name : ATM in Special

--- LabSpecial/ATM/src/Constaint/Money.java
+++ LabSpecial/ATM/src/Constaint/Money.java
@@ -1,10 +1,10 @@
 package Constaint;
 
 public class Money {
-    final int NAM_TRAM = 500000;
-    final int HAI_TRAM = 200000;
-    final int MOT_TRAM = 100000;
-    final int NAM_MUOI = 50000;
-    final int HAI_MUOI = 20000;
-    final int MUOI = 10000;
+    public int NAM_TRAM = 500000;
+    public int HAI_TRAM = 200000;
+    public int MOT_TRAM = 100000;
+    public int NAM_MUOI = 50000;
+    public int HAI_MUOI = 20000;
+    public int MUOI = 10000;
 }
--- LabSpecial/ATM/src/MAIN.java
+++ LabSpecial/ATM/src/MAIN.java
@@ -1,9 +1,15 @@
+import Components.Calculate;
+import Constaint.Money;
 import Introduce.myIntroduce;
-
 public class MAIN {
     public static void main(String[] args) {
         //Introduce
         myIntroduce rem = new myIntroduce();
         rem.Introduce();
+        //Render
+        Money m = new Money();
+        Calculate render = new Calculate();
+        render.inputMoney();
+
     }
 }

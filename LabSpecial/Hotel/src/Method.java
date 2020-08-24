8c5d4896d99a7e3033d6f293c2bce02133a08535
RemAlw <nhoxtvt@gmail.com>
2020-08-24 22:38:25 +0700

    Add value for NVTT

--- LabSpecial/Hotel/src/Method.java
+++ LabSpecial/Hotel/src/Method.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public interface  Method{
     void input();
     void output();
+    Scanner sc = new Scanner(System.in);
 }
--- LabSpecial/Hotel/src/NVTT.java
+++ LabSpecial/Hotel/src/NVTT.java
@@ -1,9 +1,26 @@
-public class NVTT {
+public class NVTT extends NhanVien implements Method{
+    private String  chucvu;
 
+    NVTT(){
+        super();
+        chucvu = null;
+    }
+    NVTT(String t,int age,String ma,String cv){
+        super(t,age,ma);
+        chucvu =cv;
+    }
+    @Override
+    public void input() {
+        super.input();
+        sc.nextLine();
+        System.out.print("\t\t\tNhập chức vụ nhân viên :");
+        chucvu = sc.nextLine();
+    }
 
-
-//    System.out.print("\t\t\tNhập chức vụ nhân viên :");
-//    chucvu = sc.nextLine();
-//            System.out.print("\t\t\tChức vụ nhân viên :"+chucvu);
+    @Override
+    public void output() {
+        super.output();
+        System.out.println("\t\t\tChức vụ nhân viên :"+chucvu);
+    }
 
 }
